05b5f9fd7bacdb1a56a4e87c51c5a1936801df3b
rodrigo.a.lopes <rodrigo.a.lopes@itamaraty.gov.br>
2018-07-31 18:25:41 -0400

    initial commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: 'AIzaSyAzUVVFeGYY8mDuJ3m1gsSO5wZ5FVt9CTE',
+        authDomain: 'monogamic-9999f.firebaseapp.com',
+        databaseURL: 'https://monogamic-9999f.firebaseio.com',
+        projectId: 'monogamic-9999f',
+        storageBucket: 'monogamic-9999f.appspot.com',
+        messagingSenderId: '932267603049'
+    }
+};
